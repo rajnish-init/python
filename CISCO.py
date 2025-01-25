c7abd4b3491cdcf53ae5ace1c35c39d2692e80e2
rajnish-init <rajnishanand333@gmail.com>
2025-01-25 23:12:52 +0530

    add CISCO python learning

---
+++ CISCO.py
@@ -0,0 +1,80 @@
+#ways to produces newline
+
+# print("The itsy bitsy spider\nclimbed up the waterspout.") #a newline character
+# print()
+# print("Down came the rain\nand washed the spider out.")
+# print("The itsy bitsy spider\\climbed up the waterspout.") #If you want to put just one backslash inside a string, don't forget its escaping nature ‒ you have to double it. 
+# #multiple arguments
+
+# print("itsy bitsy","spider climbed up","the waterspout.")
+
+# #keyword arguments
+# print("My name is", "Python.", end=" ")
+# print("Monty Python.")
+
+# print("my name is raj", end="")
+# print("raj.")
+
+# print("My", "name", "is", "Monty", "Python.", sep="-")
+
+# print("my", "name","is","raj", sep="-")
+
+# #mixed in one invocation
+# print("My", "name", "is", sep="_", end="*")
+# print("Monty", "Python.", sep="*", end="*\n")
+
+
+print("    *      "*3)
+print("   * *     "*3)
+print("  *   *    "*3)
+print(" *     *   "*3)
+print("***   ***  "*3)
+print("  *   *    "*3)
+print("  *   *    "*3)
+print("  *****    "*3)
+
+
+print("          *")
+print("        *   *")
+print("       *     *")
+print("      *       *")
+print("     *         *")
+print("    *           *")
+print("   *             *")
+print("  *               *")
+print(" *                 *")
+print("******         ******")
+print("     *         *")
+print("     *         *")
+print("     *         *")
+print("     *         *")
+print("     *         *")
+print("     ***********")
+
+
+print("    *\n   * *\n  *   *\n *     *\n***   ***")
+print("")
+
+print("My\nname\nis\nBond.", end=" ")
+print("'James Bond.'")
+
+print('Greg\'s book.')
+print("'Greg's book.'")
+print('"Greg\'s book."')
+print("Greg\'s book.")
+print("Greg\'s book.") 
+
+print("2")
+print(2)
+
+#Integers
+print(0o123)#octaldecinal
+print(0x123)#hexadecimal
+
+#Strings & escape character
+print("I like \"Monty Python\"")
+
+#an apostrophe instead of a quote
+print('I like "Monty Python"')
+
+
