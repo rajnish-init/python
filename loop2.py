ca572299d09201b064117e457110e3b9baedbc56
rajnish-init <rajnishanand333@gmail.com>
2025-03-14 12:42:49 +0530

    update python

---
+++ loop2.py
@@ -0,0 +1,73 @@
+#Break & continue
+
+for i in range(1,11):
+    
+    if i == 7:
+        print("breaking loop at", i)
+        break
+    print(i)
+
+for i in range(1,11):
+    if i == 7:
+        print("breaking loop at", i)
+        break
+    print(i)
+
+for i in range(1,11):
+    if i ==7:
+        print("breaking loop at",i)
+        break
+    print(i)
+
+for i in range(1,11):
+    if i == 8:
+        print("breaking loop at", i)
+        break
+    print(i)
+
+for i in range(1,11):
+    if i == 3:
+        print("breaking loop at", i)
+        break
+    print(i)
+
+for i in range(1,10):
+    if i == 7:
+        print("skipping", i)
+        continue
+    print(i)
+
+for i in range(1,9,2):
+    if i == 3:
+        print("skipping", i)
+        continue
+    print(i)
+
+# n = int(input("enter a number: "))
+# for i in range(1,11):
+#     print(n*i)
+
+for i in range(1,10):
+    if i ==3:
+        print("breaking loop at", i)
+        break
+    print(i)
+
+for i in range(2,15):
+    if i== 11:
+        continue
+    print(i)
+
+for i in range(1,12):
+    if i == 9:
+        continue
+    print(i)
+
+for i in range(1,9):
+    if i == 3:
+        print("skipping at", i)
+        continue
+    print(i)
+
+
+
---
+++ practice03.py
@@ -0,0 +1,77 @@
+# print("hello world!")
+# name = "raj"
+# age = 24
+# print('my name is', name, 'i am',age, 'years old')
+# age2 = age, name
+# print(age2)
+
+# def avg_3(a,b,c):
+#     avg = (a+b+c)//3
+#     print(avg)
+
+# avg_3(5,7,7)
+
+
+# def greet(name,age):
+#     name = "rajnish"
+#     age = 25
+#     print(f"hello, my name is {name}, and i am {age} years old")
+#     return name, age
+
+# greet("rajnish",25)
+
+# i =1
+# while i<=10:
+#     print(9*i)
+#     i+=1
+
+# for i in range(1,11):
+#     print(8*i)
+    
+# for i in range(1,11):
+#     print(12*i)
+
+# nums = [1,2,3,4,5,6,7,8,9,6,6]
+# x=6
+# i=0 #initialisation 
+# while i<len(nums):
+#     if nums[i]==x:
+#         print("found at index",i)
+#     i+=1
+#     print(i)
+
+# nums = (1,2,3,4,5,6,7,8,34,324,65)
+# x= 6
+# i = 0
+# while i<len(nums):
+#     if nums[i]==x:
+#         print("found at index", i)
+#     i +=1
+
+
+nums = (1,2,3,4,5,6,7,8,34,324,65)
+x = 7
+# i =0
+# while i<len(nums):
+#     if nums[i] ==x:
+#         print("found at index", i)
+#         continue 
+#     i+=1
+#     print(i)
+# else:
+#     print("not found")
+
+
+# print("Greg"); print("Python")
+
+
+# print("i, am, raj" ,sep="--")
+# print("i am raj")
+
+name = "raj"
+age = 24
+name = "rajnish"
+print(f"my name is {name} and i am {age} years old")
+
+name = "anand"
+print(f"my name is {name} and i am {age} years old")
